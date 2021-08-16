620c7a528833b5c779111515f25c412b9f85ef85
Dan Boudet <dboudet04@gmail.com>
2021-08-16 01:23:38 -0400

    improved image URLs - trimmed those with extra after .png so they display now. not perfect for those with no images whatsoever though...

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,20 +1,19 @@
 export default function Results({char}) {
     const { images, name, age, species, gender, occupation, sayings } = char
-    
+    const newImageUrl = !images ? '' : images.main.replace(new RegExp(/\.png.*/),'.png')
+
     return(
         <div className="results">
-            {char.images !== undefined && 
                 <img 
-                    src={images.main} 
+                    src={newImageUrl} 
                     alt={`${name.first} ${name.middle} ${name.last}`}
                 />
-            }
             <h2>{name.first} {name.middle} {name.last}</h2>
                 {char.age && <p><strong>Age:</strong> {age}</p>}
                 {char.species && <p><strong>Species:</strong> {species}</p>}
                 {char.gender && <p><strong>Gender:</strong> {gender}</p>}
                 {char.occupation && <p><strong>Occupation:</strong> {occupation}</p>}
-                {char.sayings && <p><strong>Sayings:</strong><br/>{sayings.slice(0,5).map( (saying) => <p className="saying">{saying}</p>)}</p>}
+                {char.sayings && <p><strong>Sayings:</strong><br/>{sayings.slice(0,5).map( (saying) => <span className="saying">{saying}<br/></span>)}</p>}
         </div>
         )
 }
